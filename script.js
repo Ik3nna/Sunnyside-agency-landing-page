db0991296dd82e6e17ba30b65bdf015219276eff
Ikenna <101594456+Ik3nna@users.noreply.github.com>
2022-08-08 19:09:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const menu = document.querySelector(".hamburger-menu");
+
+const wrapper = document.querySelector("#wrapper");
+
+const navbar = document.querySelector(".navbar");
+
+const toplink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+
+    if (scrollHeight > navHeight) {
+        wrapper.classList.add("fixed-nav");
+    } else {
+        wrapper.classList.remove("fixed-nav");
+    }
+
+    if (scrollHeight > 500) {
+       toplink.classList.add("show-link");
+    } else {
+        toplink.classList.remove("show-link");
+    }
+});
+
+menu.addEventListener("click", function () {
+    wrapper.classList.toggle("active");
+});
+
